1f2542099c57464d24015465c4e7a2821f18ee73
Santiago Astini <santiastini95@gmail.com>
2022-04-24 09:42:32 -0300

    done cards start footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+
+  // if (document.getElementById("show-prj__bk").style.width > document.getElementById("show-prj__bk").style.height) {
+  //   document.getElementById("show-prj__bk__img").style.maxWidth = "100%";
+  //   document.getElementById("show-prj__bk__img").style.maxheight = "none";
+  //   } else {
+  //     document.getElementById("show-prj__bk__img").style.maxWidth = "none";
+  //     document.getElementById("show-prj__bk__img").style.maxHeight = "100%";  
+  //   }
+
+// var width = document.getElementById("show-prj__bk").style.width
+
+//     if (document.getElementById("show-prj__bk").offsetWidth > document.getElementById("show-prj__bk").offsetHeight) {
+//       document.getElementById("show-prj__bk__img").style.maxWidth = "100%";
+//       document.getElementById("show-prj__bk__img").style.maxheight = "none";
+//       } else {
+//         document.getElementById("show-prj__bk__img").style.maxWidth = "none";
+//         document.getElementById("show-prj__bk__img").style.maxHeight = "100%";  
+//       }
